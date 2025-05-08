67c25576a38ecccb6d97895eac5f778ef1f02abb
AdamE <xadamx054@gmail.com>
2025-05-08 22:26:05 +0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "Spongebob song":return hex`0078000408040100001c00010a006400f401640000040000000000000000000000000005000004540000000400012504000800012408000c0001250c001000012910001400012520002400011e30003400012240004400012544004800012448004c0001254c005000012950005400012560006400012270007400011e`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
